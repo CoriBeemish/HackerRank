2bff2223af117fa7905f17ac586db8cfe4d292ad
CoriBeemish <cbeemish@gmail.com>
2018-03-11 18:47:49 -0400

    Create Input-and-Output.cpp

---
+++ Practice/C++/Classes/Input-and-Output.cpp
@@ -0,0 +1,16 @@
+//https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    double a, b, c, sum;
+    cin >> a >> b >> c;
+    sum = a + b + c; 
+    cout << sum;
+    return 0;
+}
