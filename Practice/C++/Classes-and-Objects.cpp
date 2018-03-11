dea2e514e08cd24030380746bf3fc2f91764f385
CoriBeemish <cbeemish@gmail.com>
2018-03-11 19:20:09 -0400

    Create Classes-and-Objects.cpp

---
+++ Practice/C++/Classes-and-Objects.cpp
@@ -0,0 +1,53 @@
+//https://www.hackerrank.com/challenges/classes-objects/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+class Student{
+    public:
+        void input(){
+            for (int i = 0; i < 5; i++){
+                int scores;
+                cin >> scores;
+                sum += scores;
+            }
+        }
+        int calculateTotalScore(){
+            return sum;
+        }
+
+    private:
+        int sum = 0;
+};
+
+int main() {
+    int n; // number of students
+    cin >> n;
+    Student *s = new Student[n]; // an array of n students
+    
+    for(int i = 0; i < n; i++){
+        s[i].input();
+    }
+
+    // calculate kristen's score
+    int kristen_score = s[0].calculateTotalScore();
+
+    // determine how many students scored higher than kristen
+    int count = 0; 
+    for(int i = 1; i < n; i++){
+        int total = s[i].calculateTotalScore();
+        if(total > kristen_score){
+            count++;
+        }
+    }
+
+    // print result
+    cout << count;
+    
+    return 0;
+}
