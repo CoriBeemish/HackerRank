0558c371e27ce61ca0c51e68592346dc7c7f8c5e
CoriBeemish <cbeemish@gmail.com>
2018-03-26 20:49:01 -0400

    Create Print-Function.py

---
+++ Practice/Python/Print-Function.py
@@ -0,0 +1,7 @@
+#https://www.hackerrank.com/challenges/python-print/problem
+
+from __future__ import print_function
+if __name__ == '__main__':
+    n = int(raw_input())
+    for n in range(n):
+        print (n + 1, end='')
