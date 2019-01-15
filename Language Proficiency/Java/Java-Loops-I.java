dae8d9f7946d427a0cfdff9c3d543ba6db2a01bc
Cori Beemish <cbeemish@gmail.com>
2019-01-15 18:17:53 -0500

    Updating HackerRank Problems

---
+++ Language Proficiency/Java/Java-Loops-I.java
@@ -0,0 +1,22 @@
+// Completed on January 15th, 2019
+
+// Java Loops I
+// https://www.hackerrank.com/challenges/java-loops-i/problem
+
+import java.util.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        scanner.close();
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
+
+
